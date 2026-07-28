c5a614adca4ef16aae7227219cdb30640832b913
mynamesdallas <62816012+mynamesdallas@users.noreply.github.com>
2026-07-27 22:13:23 -0400

    Refined some css to make modal sit center of screen and added the inputs for the modal for supervisors to add information for employee

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     modal.close();
 })
+
+const workTitleInput = document.createElement("input");
+workTitleInput.type = "text";
+workTitleInput.className = "work-title-input";
+workTitleInput.placeholder = "Enter work here...";
+
+const employeeInput = document.createElement("input");
+employeeInput.type = "text";
+employeeInput.className = "employee-input";
+employeeInput.placeholder = "Assigned to...";
+
+const locationInput = document.createElement("input");
+locationInput.type = "text";
+locationInput.className = "location-input";
+locationInput.placeholder = "Enter job address";
+
+const notesInput = document.createElement("textarea");
+notesInput.className = "notes-input";
+notesInput.placeholder = "Specific notes for employee..."
+
+modal.append(workTitleInput, employeeInput, locationInput, notesInput)
