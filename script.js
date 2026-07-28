01d1879228ec26a52d0a487dda48007bf666fcff
mynamesdallas <62816012+mynamesdallas@users.noreply.github.com>
2026-07-28 00:01:41 -0400

    Started working on the output display for the modal inputs

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const modal = document.querySelector("#modalContainer");
 const modalInputs = document.querySelector(".modal-body");
 const openBtn = document.querySelector("#openModalBtn");
 const closeBtn = document.querySelector("#closeModalBtn");
+const modalFooter = document.querySelector(".modal-footer");
+const outputContainer = document.querySelector("#outputContainer");
 
 openBtn.addEventListener("click", () => {
     modal.showModal()
@@ -28,8 +30,35 @@ locationInput.placeholder = "Enter job address";
 
 const notesInput = document.createElement("textarea");
 notesInput.className = "notes-input input";
-notesInput.placeholder = "Specific notes for employee..."
+notesInput.placeholder = "Specific notes for employee...";
+
+const sendTask = document.createElement("button");
+sendTask.id = "sendTaskBtn";
+sendTask.textContent = "Assign Task";
+
+sendTask.addEventListener("click", () => {
+    const workTitle = workTitleInput.value;
+    workTitleInput.value = "";
+    const assignedTo = employeeInput.value;
+    employeeInput.value = "";
+    const location = locationInput.value;
+    locationInput.value = "";
+    const notes = notesInput.value;
+    notesInput.value = "";
+
+    const outputDisplay = document.querySelector("div");
+    outputDisplay.className = "output-display";
+    const titleDisplay = document.createElement("h2");
+
+    outputDisplay.appendChild(titleDisplay)
+    titleDisplay.textContent = workTitle;
+
+
+    
+})
+
+modalFooter.appendChild(sendTask)
 
 modalInputs.append(workTitleInput, employeeInput, locationInput, notesInput);
 
-modal.appendChild(modalInputs)
+modal.append(modalInputs, modalFooter)
