8624b38a566befcd795e801cc6f78c89acfa3a4e
mynamesdallas <62816012+mynamesdallas@users.noreply.github.com>
2026-07-27 22:55:45 -0400

    Moved where my inputs were appended to and flexed the input in the container to make them more responsive.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const modal = document.querySelector("#modalContainer");
+const modalInputs = document.querySelector(".modal-body");
 const openBtn = document.querySelector("#openModalBtn");
 const closeBtn = document.querySelector("#closeModalBtn");
 
@@ -12,21 +13,23 @@ closeBtn.addEventListener("click", () => {
 
 const workTitleInput = document.createElement("input");
 workTitleInput.type = "text";
-workTitleInput.className = "work-title-input";
+workTitleInput.className = "work-title-input, input";
 workTitleInput.placeholder = "Enter work here...";
 
 const employeeInput = document.createElement("input");
 employeeInput.type = "text";
-employeeInput.className = "employee-input";
+employeeInput.className = "employee-input input";
 employeeInput.placeholder = "Assigned to...";
 
 const locationInput = document.createElement("input");
 locationInput.type = "text";
-locationInput.className = "location-input";
+locationInput.className = "location-input input";
 locationInput.placeholder = "Enter job address";
 
 const notesInput = document.createElement("textarea");
-notesInput.className = "notes-input";
+notesInput.className = "notes-input input";
 notesInput.placeholder = "Specific notes for employee..."
 
-modal.append(workTitleInput, employeeInput, locationInput, notesInput)
+modalInputs.append(workTitleInput, employeeInput, locationInput, notesInput);
+
+modal.appendChild(modalInputs)
