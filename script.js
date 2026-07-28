8ecad59f2006578885adc49373b2e940c472487b
myNamesDallas <62816012+mynamesdallas@users.noreply.github.com>
2026-07-27 21:01:44 -0400

    Added a few more things to the modal to get it looking like a functioning input grabber in script and did some styling in css

--- script.js
+++ script.js
@@ -9,15 +9,3 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     modal.close();
 })
-
-modal.addEventListener('click', (e) => {
-  const dialogDimensions = modal.getBoundingClientRect();
-  if (
-    e.clientX < dialogDimensions.left ||
-    e.clientX > dialogDimensions.right ||
-    e.clientY < dialogDimensions.top ||
-    e.clientY > dialogDimensions.bottom
-  ) {
-    modal.close();
-  }
-});
